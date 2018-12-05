e76a72cf2da9c4f8a27693acf0e048e99f768934
Zhian N. Kamvar <zkamvar@gmail.com>
2018-12-05 21:01:38 +0000

    add guessing_game (rip old commits)

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,38 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+
+    loop {
+        println!("Please enter your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line!!!!!11one!!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_)  => continue,
+        };
+
+        println!("You guessed {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("this porridge is too cold!"),
+            Ordering::Greater => println!("this porridge is too hot!"),
+            Ordering::Equal   => {
+                println!("this porridge is ~~just right~~");
+                break;
+            },
+        }
+    }
+}
