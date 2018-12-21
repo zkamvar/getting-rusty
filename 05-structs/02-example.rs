96de869e0674e819a3a94547d46eee3e56527d40
Zhian N. Kamvar <zkamvar@gmail.com>
2018-12-21 16:50:41 +0900

    add debugging statement

--- 05-structs/02-example.rs
+++ 05-structs/02-example.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -5,7 +6,7 @@ struct Rectangle {
 
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
-
+    println!("DEBUG:\trect is {:?}", rect1);
     println!(
         "The area of the rectangle is {} square pixels.",
         area(&rect1)
