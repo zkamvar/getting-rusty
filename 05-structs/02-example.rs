199725693b5cd56f7335538783d84e91c78eb582
Zhian N. Kamvar <zkamvar@gmail.com>
2018-12-21 16:51:29 +0900

    print larger struct for debugging

--- 05-structs/02-example.rs
+++ 05-structs/02-example.rs
@@ -6,7 +6,7 @@ struct Rectangle {
 
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
-    println!("DEBUG:\trect is {:?}", rect1);
+    println!("DEBUG:\trect is {:#?}", rect1);
     println!(
         "The area of the rectangle is {} square pixels.",
         area(&rect1)
