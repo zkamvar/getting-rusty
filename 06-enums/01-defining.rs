e7ee905f213c8f38dec8add61cc0d823b7eeb492
Zhian N. Kamvar <zkamvar@gmail.com>
2018-12-21 17:44:17 +0900

    show that different values can be used inside enums

--- 06-enums/01-defining.rs
+++ 06-enums/01-defining.rs
@@ -1,9 +1,10 @@
 // Unlike structs, enums define the *possible* values something could take. This
 // allows us to define one function that can take either type, but still have
 // the same signature.
-enum IpAddrKind {
-    V4, // V4 variant
-    V6, // V6 variant
+enum IpAddr {
+    V4(u8, u8, u8, u8), // V4 variant
+    V6(String), // V6 variant
 }
-// let V4 = IpAddrKind::V4;
-// let V6 = IpAddrKind::V6;
+// let home = IpAddr::V4(127, 0, 0, 0);
+//
+// let loopback = IpAddr::V6(String::from("::1"));
