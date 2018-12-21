d5af45fbc9cde26e35303345c3ea653269911fbd
Zhian N. Kamvar <zkamvar@gmail.com>
2018-12-21 18:14:22 +0900

    add option enum

--- 06-enums/01-defining.rs
+++ 06-enums/01-defining.rs
@@ -2,9 +2,29 @@
 // allows us to define one function that can take either type, but still have
 // the same signature.
 enum IpAddr {
-    V4(u8, u8, u8, u8), // V4 variant
+    V4(u8, u8, u8, u8), // V4 variant: u8 forces the integer to be between 0 and 255
     V6(String), // V6 variant
 }
 // let home = IpAddr::V4(127, 0, 0, 0);
 //
 // let loopback = IpAddr::V6(String::from("::1"));
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 }, // this includes an anonymous struct
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+// Enums can also have methods associated
+
+
+// The Option enum ------------------------------------------------------------
+// 
+// This is an enum that allows for some or none of something to be defined:
+
+fn main() {
+    let some_number = Some(5);
+    let some_string = Some("a string");
+    let absent_number: Option<i32> = None; // This is none for now, but could be an i32 later
+}
