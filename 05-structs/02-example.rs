0256e1f60fb975bc724bd415e800021ff69779a2
Zhian N. Kamvar <zkamvar@gmail.com>
2018-12-21 17:11:08 +0900

    add square implementation

--- 05-structs/02-example.rs
+++ 05-structs/02-example.rs
@@ -1,8 +1,10 @@
 #[derive(Debug)]
+// Struct -------------------------
 struct Rectangle {
     width: u32,
     height: u32,
 }
+// Methods ------------------------
 impl Rectangle {
     // Note: methods can take ownership, borrow immutably (here), and
     //       borrow mutably. Since we don't want to harm the Rectangle itself,
@@ -18,10 +20,20 @@ impl Rectangle {
         self.width > other.width && self.height > other.height
     }
 }
+// Associated functions -----------
+impl Rectangle {
+    fn square (size: u32) -> Rectangle {
+        Rectangle {width: size, height: size}
+    }
+}
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
     let rect2 = Rectangle { width: 10, height: 40 };
     let rect3 = Rectangle { width: 60, height: 45 };
+    
+    // Using an associated function
+    let square1 = Rectangle::square(20);
+    
     println!("DEBUG:\trect is {:#?}", rect1);
     println!(
         "The area of the rectangle is {} square pixels.",
@@ -29,4 +41,5 @@ fn main() {
     );
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("Can rect1 hold a {} px square? {}", square1.area(), rect1.can_hold(&square1));
 }
