8d821ac9ff6dc00293367fccd6490784d11a181a
Zhian N. Kamvar <zkamvar@gmail.com>
2018-12-22 22:03:20 +0900

    add explanation for private structs

--- 07-Packages-Crates-Modules/my-project/src/main.rs
+++ 07-Packages-Crates-Modules/my-project/src/main.rs
@@ -2,7 +2,7 @@ mod sound {
     pub mod instrument {
         pub mod woodwind {
             pub fn clarinet() {
-                super::breathe_in();
+                super::breathe_in(); // super is equivalent to '..' in UNIX
                 println!("toot");
             }
         }
@@ -21,8 +21,31 @@ mod sound {
         }
     }
 }
+
+mod plant {
+    #[derive(Debug)]
+    pub struct Vegetable {
+        pub name: String, // only this part of the struct is visible
+        id: i32,
+    }
+    impl Vegetable {
+        pub fn new(name: &str) -> Vegetable {
+            Vegetable {
+                name: String::from(name),
+                id: 1,
+            }
+        }
+    }
+}
 fn main() {
-    crate::sound::instrument::woodwind::clarinet();
-    sound::instrument::string::guitar();
+    // Part 1
+    crate::sound::instrument::woodwind::clarinet(); // absolute path
+    sound::instrument::string::guitar();            // relative path
     sound::voice::soprano();
+    // Part 2
+    let mut v = plant::Vegetable::new("squash");
+    v.name = String::from("Butternut squash");
+    println!("{} are delicious!", v.name);
+    println!("The vegetable struct is\n{:#?}\nThe name is public, but the id is private", v);
+    // println!("ID: {}", v.id); // id is private
 }
