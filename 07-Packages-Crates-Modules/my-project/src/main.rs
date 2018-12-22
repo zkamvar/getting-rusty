11695d8a67325fcac4b81657eb0954629db47035
Zhian N. Kamvar <zkamvar@gmail.com>
2018-12-22 21:28:02 +0900

    add nested modules

--- 07-Packages-Crates-Modules/my-project/src/main.rs
+++ 07-Packages-Crates-Modules/my-project/src/main.rs
@@ -1,3 +1,22 @@
+mod sound {
+    mod instrument {
+        mod woodwind {
+            fn clarinet() {
+                println!("toot");
+            }
+        }
+        mod string {
+            fn guitar() {
+                println!("meedlymeeldymeedlyWAAAAAAAA");
+            }
+        }
+    }
+    mod voice {
+        fn soprano () {
+          println!("EEEEE");  
+        }
+    }
+}
 fn main() {
     println!("Hello, world!");
 }
