cb6c3dca363ca0fac054c069e6d292def3454371
Zhian N. Kamvar <zkamvar@gmail.com>
2018-12-22 13:02:35 +0900

    add _ example

--- 06-enums/02-match.rs
+++ 06-enums/02-match.rs
@@ -15,10 +15,10 @@ enum Coin {
 
 fn value_in_cents (coin: Coin) -> u32 {
     match coin {
-        Coin::Penny   =>  1,
-        Coin::Nickle  =>  5,
-        Coin::Dime    => 10,
-        Coin::Quarter(state)   =>  { // patterns can bind to values
+        Coin::Penny  =>  1,
+        Coin::Nickle =>  5,
+        Coin::Dime   => 10,
+        Coin::Quarter(state) =>  { // patterns can bind to values
             println!("State quarter from {:?}", state);    
             25
         },
@@ -29,7 +29,7 @@ fn value_in_cents (coin: Coin) -> u32 {
 // Matching using Option<T> ----------------------------------------------------
 fn plus_one(x: Option<i32>) -> Option<i32> {
     match x {
-        None    => None,
+        None    => None, // This part is necessary because matching is exhaustive
         Some(i) => Some(i + 1),
     }
 }
@@ -38,4 +38,12 @@ fn main() {
     let five = Some(5);
     let six  = plus_one(five);
     let none = plus_one(None);
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (), // _ matches the rest and () are the goggles
+    }
 }
