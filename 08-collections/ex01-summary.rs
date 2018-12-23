94cd46cfa08b304e45c54d55219b355c42272a91
Zhian N. Kamvar <zkamvar@gmail.com>
2018-12-23 20:06:52 +0900

    add cli support

--- 08-collections/ex01-summary.rs
+++ 08-collections/ex01-summary.rs
@@ -1,13 +1,19 @@
 use std::collections::HashMap;
 use std::str::FromStr;
+use std::io;
 
 fn main () {
-    let s = "5 4 2 3 2 2 2 2 1 5 2 50".to_string();
+    let mut s = String::new();
+    // shuf -r -i 1-10 -n 10 | tr '\\n' ' ' | ./ex01-summary
+    io::stdin().read_line(&mut s)
+        .expect("I got nothing");
+    // let s = "5 4 2 3 2 2 2 2 1 5 2 50".to_string();
+    
     let mut v = Vec::new();
     for number in s.split_whitespace() {
         v.push(i32::from_str(number).unwrap());
     }
-    println!("{:?}", v);
+    // println!("{:?}", v);
     let (mean, median, mode) = mean_median_mode(&v);
     v.sort_unstable();
     println!("mean: {}, median: {}, mode: {}", mean, median, mode);
@@ -17,7 +23,7 @@ fn mean_median_mode(v: &Vec<i32>) -> (f64, i32, i32) {
     let mut modemap = HashMap::new();
     let denom = v.len() as f64;
     let mut numerator = 0;
-    let mut med = (denom / 2.0).floor() as usize;
+    let med = (denom / 2.0).floor() as usize;
     for i in v {
         numerator += i;
         let count =  modemap.entry(i).or_insert(0);
