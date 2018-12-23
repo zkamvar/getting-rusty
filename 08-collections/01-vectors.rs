21f66bdd1956f7240cb7910824d4874810843ce0
Zhian N. Kamvar <zkamvar@gmail.com>
2018-12-23 11:41:49 +0900

    add vectors with enums

--- 08-collections/01-vectors.rs
+++ 08-collections/01-vectors.rs
@@ -37,7 +37,7 @@ fn main() {
      * println!("first element is {}", first); // PANIC!
      */
 
-    // Iteration --------------------------------------------------------------
+    // Iteration ---------------------------------------------------------------
     let v = vec![100, 32, 69];
     for i in &v {
         println!("{}", i);
@@ -49,4 +49,31 @@ fn main() {
         *i += 51;
     }
     println!("after:  {:?}", v);
+
+    // Using enums to store multiple values ------------------------------------
+    // 
+    // Since enums are all of the same type, we can use them in a vector to 
+    // store multiple values.
+    #[derive(Debug)]
+    enum SC { // Spreadsheet Cell
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SC::Int(6911),
+        SC::Text(String::from("the inside")),
+        SC::Float(2.10),
+    ];
+    println!("the value of row is {:?}", row);
+    
+    // Attempt at a sort of data frame representation.
+    let ID = vec![SC::Int(69), SC::Int(11), SC::Int(29),];
+    let desc = vec![SC::Text(String::from("a")), SC::Text(String::from("b")), SC::Text(String::from("c")),];
+    let val = vec![SC::Float(1.1), SC::Float(2.2), SC::Float(3.3),];
+    
+    let dat = vec![ID, desc, val];
+
+    println!("The data is {:?}", dat);
 }
