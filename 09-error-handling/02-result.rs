d607b4c911632e23710b5846d4fb6340538ba6ad
Zhian N. Kamvar <zkamvar@gmail.com>
2018-12-24 13:52:53 +0900

    add fn to propogate errors

--- 09-error-handling/02-result.rs
+++ 09-error-handling/02-result.rs
@@ -1,11 +1,17 @@
-use std::fs::File;
+use std::io;
 use std::io::ErrorKind; // For treating different kinds of errors
+use std::io::Read;
+use std::fs::File;
 
 fn main() {
     // panic_at_the_disco();
-    wait_panic();
+    // wait_panic();
+    // use_expect();
+    let f = read_username_from_file().expect("no file!?");
+    println!("woo! {}", f);
 }
 
+// Panic if something... anything is wrong -------------------------------------
 fn panic_at_the_disco() {
     let f = File::open("hello.txt");
 
@@ -17,6 +23,7 @@ fn panic_at_the_disco() {
     };
 }
 
+// Wait and check the situation and then panic ---------------------------------
 fn wait_panic() {
     let f = File::open("hello.txt");
 
@@ -24,13 +31,37 @@ fn wait_panic() {
         Ok(file) => file,
         Err(errrr) => match errrr.kind() {
             ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(newf) => newf,
+                Ok(newf) => {
+                    println!("I just created hello.txt");
+                    newf
+                },
                 Err(bork) => {
                     panic!("There wasn't a file, so I tried to create one, but got another error: {:?}", bork);
-                }
+                },
             },
             other_error => panic!("the disco: {:?}", other_error),
         },
     };
+}
 
+// Using expect method ---------------------------------------------------------
+fn use_expect() {
+    let f = File::open("hello.txt").expect("the disco");
+}
+
+// Error propogation -----------------------------------------------------------
+// This passes the Result type to the parent function so that it can figure out
+// how best to deal with the error
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+    let mut f = match f {
+        Ok(yeah) => yeah,
+        Err(wut) => return Err(wut),
+    };
+
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(dang) => return Err(dang),
+    }
 }
