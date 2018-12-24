bb2ad5d47010c27c37098d43c711818f0fec693b
Zhian N. Kamvar <zkamvar@gmail.com>
2018-12-24 13:25:36 +0900

    add error kind handling
    I'm glad that I changed the error name to errrr or I wouldn't have
    realized that error.kind() is the kind method, not a function in and
    of itself.

--- 09-error-handling/02-result.rs
+++ 09-error-handling/02-result.rs
@@ -1,6 +1,12 @@
 use std::fs::File;
+use std::io::ErrorKind; // For treating different kinds of errors
 
 fn main() {
+    // panic_at_the_disco();
+    wait_panic();
+}
+
+fn panic_at_the_disco() {
     let f = File::open("hello.txt");
 
     let f = match f {
@@ -9,4 +15,22 @@ fn main() {
             panic!("the disco: {:?}", errrr);
         },
     };
+}
+
+fn wait_panic() {
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(errrr) => match errrr.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(newf) => newf,
+                Err(bork) => {
+                    panic!("There wasn't a file, so I tried to create one, but got another error: {:?}", bork);
+                }
+            },
+            other_error => panic!("the disco: {:?}", other_error),
+        },
+    };
+
 }
