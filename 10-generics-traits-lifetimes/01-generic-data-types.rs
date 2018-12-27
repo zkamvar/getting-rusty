b03d0f9a1524f1f5b2a613478d771d662e1c74ea
Zhian N. Kamvar <zkamvar@gmail.com>
2018-12-27 15:32:57 +0900

    add main function for finidn largest number

---
+++ 10-generics-traits-lifetimes/01-generic-data-types.rs
@@ -0,0 +1,19 @@
+// Generics, are Rust's way of handling duplicate concepts.
+//
+// This lesson will cover
+//
+// 1. How to extract a function to reduce duplication
+// 2. How construct a generic function that differ only in the parameter types
+// 3. How to use generic types in struct and enum definitions
+
+fn main() {
+    let number_list = vec![38, 203, 38, 2837, 39, 7];
+    let mut largest = number_list[0];
+
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+    println!("the largest number is {}", largest);
+}
