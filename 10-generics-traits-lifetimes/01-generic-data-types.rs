03e17d26bc592e992d983fca66d1bd2680596ff5
Zhian N. Kamvar <zkamvar@gmail.com>
2018-12-27 15:46:38 +0900

    add type-specific functions

--- 10-generics-traits-lifetimes/01-generic-data-types.rs
+++ 10-generics-traits-lifetimes/01-generic-data-types.rs
@@ -23,4 +23,32 @@ fn main() {
     let number_list = vec![02, 1, 24, 3, 5, 1, 100, 8, 3, 33, 5];
 
     println!("the largest number is {}", largest(&number_list));
+    
+    let char_list = vec!['a', 'f', 'w', 't', 'h'];
+
+    println!("the largest character is {}", largest_char(&char_list));
+    println!("the largest number is {}", largest_i32(&number_list));
+}
+
+
+// Generic Data Types in function definitions ----------------------------------
+
+fn largest_i32(list: &[i32])-> i32 {
+    let mut largest = list[0];
+    for &number in list.iter() {
+        if number > largest {
+            largest = number;
+        }
+    }
+    largest
+}
+
+fn largest_char(list: &[char])-> char {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
 }
