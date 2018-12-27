31f715f4ccafb6170c31ade038ccda5b45f8b2e0
Zhian N. Kamvar <zkamvar@gmail.com>
2018-12-27 15:39:10 +0900

    update to use borrowing instead of ownership

--- 10-generics-traits-lifetimes/01-generic-data-types.rs
+++ 10-generics-traits-lifetimes/01-generic-data-types.rs
@@ -6,9 +6,9 @@
 // 2. How construct a generic function that differ only in the parameter types
 // 3. How to use generic types in struct and enum definitions
 
-fn largest(list: Vec<i32>) -> i32 {
+fn largest(list: &[i32])-> i32 {
     let mut largest = list[0];
-    for number in list {
+    for &number in list.iter() {
         if number > largest {
             largest = number;
         }
@@ -18,9 +18,9 @@ fn largest(list: Vec<i32>) -> i32 {
 
 fn main() {
     let number_list = vec![38, 203, 38, 2837, 39, 7];
-    println!("the largest number is {}", largest(number_list));
+    println!("the largest number is {}", largest(&number_list));
 
     let number_list = vec![02, 1, 24, 3, 5, 1, 100, 8, 3, 33, 5];
 
-    println!("the largest number is {}", largest(number_list));
+    println!("the largest number is {}", largest(&number_list));
 }
