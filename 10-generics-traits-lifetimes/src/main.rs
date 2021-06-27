f454144ef92ec20df6c1440c26031b5bd8068ecd
Zhian N. Kamvar <zkamvar@gmail.com>
2021-06-26 18:28:29 -0700

    finish generics; move files;

--- 10-generics-traits-lifetimes/src/main.rs
+++ 10-generics-traits-lifetimes/src/main.rs
@@ -39,6 +39,25 @@ fn largest_char(list: &[char])-> char {
     largest
 }
 
+// Type naming conventions ----------------------------------------------------
+//
+// Rust's Type naming conventions are CamelCase, but often the default will be
+// just a single letter, "T" for "Type".
+//
+// NOTE: This will _not_ compile because of the fact that we use the greater 
+// than operator, which needs a type that can be ordered. At the moment, our 
+// definition allows _ANYTHING_ including unordered types.
+
+// fn largest<T>(list: &[T]) -> T {
+//     let mut largest = list[0];
+//     for &number in list.iter() {
+//         if number > largest {
+//             largest = number;
+//         }
+//     }
+//     largest
+// }
+
 // Structs and enums ----------------------------------------------------------
 
 // A struct can be generic, but if it has one generic type, all types must be
@@ -54,22 +73,45 @@ struct Point<T, U> {
     y: U,
 }
 
+// Method definitions
+// generic type methods
+impl<T> Pint<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+// concrete methods act on specific type
+impl Pint<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+// Traits ---------------------------------------------------------------------
+
+
+
 fn main() {
+    // Functions ---
     let number_list = vec![38, 203, 38, 2837, 39, 7];
     println!("the largest number is {}", largest(&number_list));
 
     let number_list = vec![02, 1, 24, 3, 5, 1, 100, 8, 3, 33, 5];
-
     println!("the largest number is {}", largest(&number_list));
-    
-    let char_list = vec!['a', 'f', 'w', 't', 'h'];
 
+    let char_list = vec!['a', 'f', 'w', 't', 'h'];
     println!("the largest character is {}", largest_char(&char_list));
     println!("the largest number is {}", largest_i32(&number_list));
 
+    // Structs ---
     // all types must be the same if the struct only has one type
     let pint = Pint { x: 5, y: 10};
-    let pint = Pint { x: 5.0, y: 10.0};
+    // Generic method works, but concrete method not defined for i32
+    println!("pint.x = {}", pint.x());
+    // println!("distance from origin: {}", pint.distance_from_origin());
+    let pint = Pint { x: 3.0, y: 4.0};
+    println!("pint.x = {}", pint.x());
+    println!("distance from origin: {}", pint.distance_from_origin());
     // let wont_work = Pint { x: 5.0, y: 10 }`
 
     let point = Point { x: 5, y: 10};
