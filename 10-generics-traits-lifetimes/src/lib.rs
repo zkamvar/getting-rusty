a6f935c8a6c47aa5a864dae9a2d532b2c01b3047
Zhian N. Kamvar <zkamvar@gmail.com>
2021-06-27 13:52:23 -0700

    Add traits as parameters

--- 10-generics-traits-lifetimes/src/lib.rs
+++ 10-generics-traits-lifetimes/src/lib.rs
@@ -44,3 +44,60 @@ impl Summary for Tweet {
         format!("from   : {}\nmessage: \"{}\"", self.username, self.content)
     }
 }
+
+
+// Traits as parameters (in functions) ----------------------------------------
+//
+// This allows us to create more generic functions that can call methods on
+// any type that implements the Summary trait. This means that we can write 
+// functions specific for our types, but these functions will cease to work
+// if String or i32 types are passed.
+
+// impl Trait syntax ~~~
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// Trait bound syntax ~~~
+//
+// The `impl Trait` syntax is sugar for the trait bound syntax. So the above 
+// code is equivalent to: 
+pub fn notify<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// Benefits of Trait bound syntax ~~~
+//
+// The trait bound syntax is useful for more complex situations where you would
+// have more than one argument in your function:
+//
+//      pub fun notify(item1: &impl Summary, item2: &impl Summary)
+// vs
+//      pub fun notify<T: Summary>(item1: &T, item2: &T)
+
+// Multiple Traits ~~~
+//
+// We can use the `+` syntax to allow for multiple traits in a single parameter
+//
+//      pub fn notify(item: &(impl Summary + Display))
+// vs
+//      pub fn notify<T: Summary + Display>(item: &T)
+//
+
+// Clearer Trait Bounds with `where` Clauses ~~~
+//
+// The Trait bound syntax really shines when you need to have multiple possible
+// traits for individual parameters to avoid your functions from becoming 
+// overburdened in the definition:
+//
+//      fn some_function<T: Display + Clone, U: Clone + Debug>(t: &T, u &U) -> i32 {
+//          // Function Body
+//      }
+// is equivalent to
+//      fn some_function<T, U>(t: &T, u: &U) -> i32
+//          where T: Display + Clone,
+//                U: Clone + Debug
+//      {
+//          // Function Body
+//      }
+//
