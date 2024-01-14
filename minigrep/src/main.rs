1b0d6ec2c61bc06bf87f7796f8897ab6cd5d3d08
Zhian N. Kamvar <zkamvar@gmail.com>
2024-01-14 11:06:37 +0900

    refactor into lib

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,5 @@
+use minigrep::Config;
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
 // use std::ffi;
 
@@ -19,33 +18,8 @@ fn main() {
     println!("Searching for: `{}`", config.query);
     println!("in file      :  {}", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     };
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Expected two arguments: <string> and <file>.");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
-
-fn run(cfg: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(cfg.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
 }
