8f1f6a44ed1043e2a5972bdf3e3948669d6894dc
Zhian N. Kamvar <zkamvar@gmail.com>
2024-01-14 09:26:36 +0900

    add basic tests for equality

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,13 +1,55 @@
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
+#[derive(Debug)]
+pub struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+    pub fn is_square(&self) -> bool {
+        self.width == self.height
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    #[test]
+    fn big_box_hold_little_box() {
+        let larger = Rectangle {
+            width: 6,
+            height: 9,
+        };
+        let smol = Rectangle {
+            width: 3,
+            height: 3,
+        };
+        assert!(larger.can_hold(&smol));
+        assert!(!smol.can_hold(&larger));
+        assert!(!smol.can_hold(&smol));
+        assert!(!larger.can_hold(&larger));
+    }
+    #[test]
+    fn lets_be_l7() {
+        let rekt = Rectangle {
+            width: 6,
+            height: 9,
+        };
+        let l7 = Rectangle {
+            width: 3,
+            height: 3,
+        };
+        assert!(l7.is_square());
+        assert!(!rekt.is_square());
+    }
 
     #[test]
-    fn it_works() {
+    fn addition_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
