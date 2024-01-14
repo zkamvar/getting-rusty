644ec884401e135a0ede1e31351682a961e89e70
Zhian N. Kamvar <zkamvar@gmail.com>
2024-01-14 11:26:41 +0900

    implemented word search

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -21,7 +21,35 @@ impl Config {
 pub fn run(cfg: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(cfg.file_path)?;
 
-    println!("With text:\n{contents}");
+    let mut i: u32 = 0;
+    for line in search(&cfg.query, &contents) {
+        i += 1;
+        println!("match {i}: {line}");
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
