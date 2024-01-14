25d0f18384c6e32cb587db4752363055f0e6b76e
Zhian N. Kamvar <zkamvar@gmail.com>
2024-01-14 10:16:49 +0900

    add first reading in of args with debug;

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+// use std::ffi;
+
 fn main() {
-    println!("Hello, world!");
+    // using env::args instead of just args is less ambiguous
+    // To deal with invalid unicode, use std::env::args_os, which
+    // will produce OsString values.
+    // let args: Vec<ffi::OsString> = env::args_os().collect();
+    let args: Vec<String> = env::args().collect();
+    dbg!(args);
 }
