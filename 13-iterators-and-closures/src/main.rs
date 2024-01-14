c820e7c549c6f637d93148e7312a9b9e5e7609e1
Zhian N. Kamvar <zkamvar@gmail.com>
2024-01-14 13:41:52 +0900

    begin closures

--- 13-iterators-and-closures/src/main.rs
+++ 13-iterators-and-closures/src/main.rs
@@ -1,3 +1,60 @@
+// Closures:: capturing environment and scope
+
+// Capturing the environment
+//
+// - T-shirt company gives away a special shirt to rando
+// - people can add their favorite color to profile
+// - if rando has no favorite color, they get the most stocked color
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        // Hey! This uses a closure without any arguments that returns
+        // a valute T (the Some() variant of Option<T>).
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
+        }
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
 }
