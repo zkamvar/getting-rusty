951166f39b981d998ca4ca5cf3ffa9d206c68a90
Zhian N. Kamvar <zkamvar@gmail.com>
2024-03-22 15:07:26 -0700

    start patterns got to syntax

--- 06-enums/02-match.rs
+++ 06-enums/02-match.rs
@@ -13,15 +13,16 @@ enum Coin {
     Quarter(US_State),
 }
 
-fn value_in_cents (coin: Coin) -> u32 {
+fn value_in_cents(coin: Coin) -> u32 {
     match coin {
-        Coin::Penny  =>  1,
-        Coin::Nickle =>  5,
-        Coin::Dime   => 10,
-        Coin::Quarter(state) =>  { // patterns can bind to values
-            println!("State quarter from {:?}", state);    
+        Coin::Penny => 1,
+        Coin::Nickle => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            // patterns can bind to values
+            println!("State quarter from {:?}", state);
             25
-        },
+        }
     }
 }
 // value_in_cents(Coin::Quarter(US_State::Alaska))
@@ -29,14 +30,14 @@ fn value_in_cents (coin: Coin) -> u32 {
 // Matching using Option<T> ----------------------------------------------------
 fn plus_one(x: Option<i32>) -> Option<i32> {
     match x {
-        None    => None, // This part is necessary because matching is exhaustive
+        None => None, // This part is necessary because matching is exhaustive
         Some(i) => Some(i + 1),
     }
 }
 
 fn main() {
     let five = Some(5);
-    let six  = plus_one(five);
+    let six = plus_one(five);
     let none = plus_one(None);
     let some_u8_value = 0u8;
     match some_u8_value {
--- 18-patterns/src/main.rs
+++ 18-patterns/src/main.rs
@@ -1,3 +1,246 @@
+// Patterns consist of some combination of
+//
+// - Literals
+// - Destructured arrays, enums, structs, or tuples
+// - Variables
+// - Wildcards
+// - Placeholders
+
+// Matching using Option<T> ----------------------------------------------------
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None, // This part is necessary because matching is exhaustive
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn print_coordinates(&(x, y): &(i32, i32)) {
+    println!("Current location: ({}, {})", x, y);
+}
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+    ColorChange(Color),
+}
+
+#[derive(Debug)]
+enum Color {
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("## All the Places Patterns Can Be Used\n");
+
+    // ------------------------------------------------
+    println!("---| `match` Arms");
+    let five = Some(5);
+    println!("Five plus one is {:?}", plus_one(five));
+    println!("None plus one is {:?}", plus_one(None));
+    println!();
+
+    // ------------------------------------------------
+    println!("---| Conditional `if let` Expressions");
+    let favourite_colour: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(colour) = favourite_colour {
+        println!("[if let] Using your favourite colour, {colour}, as the background");
+    } else if is_tuesday {
+        println!("[else if] Tuesday is green day!");
+    } else if let Ok(age) = age {
+        // The `age` variable is only in scope starting here.
+        // This is because `Ok(age)` is the _shadowed_ `age` variable.
+        // Not possible to do `let Ok(age) = age && age > 30`
+        if age > 30 {
+            println!("[else if let Ok()] Using purple as the background colour");
+        } else {
+            println!("[else if let Ok()] Using orange as the background colour");
+        }
+    } else {
+        // this shows that it is not exhaustive.
+        println!("[else] Using blue as the background colour");
+    }
+    println!();
+
+    // ------------------------------------------------
+    println!("---| `while let` Condidtional Loops");
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    println!("pop the top offfa {:?}", stack);
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+    println!();
+
+    // ------------------------------------------------
+    println!("---| `for` Loops");
+
+    let v = vec!['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
+    println!();
+
+    // ------------------------------------------------
+    println!("---| `let` statements");
+    println!("`let x = 5;`, the variable name is a simple PATTERN.");
+
+    println!("in tuples, the number of elements in PATTERN are checked with the EXPRESSION.");
+    let (_x, _y, _z) = (3, 4, 5);
+    println!();
+
+    // ------------------------------------------------
+    println!("---| function parameters");
+
+    let point = (3, 5);
+    print_coordinates(&point);
+    println!();
+
+    // ================================================
+    println!("## Refutability: Weather a Pattern Might Fail to Match\n");
+
+    println!("`let x` statements are irrefutable because they can match anything");
+    println!(
+        "`let (x, y)` statements are refutable because these will not match anything but a
+    two-element tuple"
+    );
+    println!();
+
+    // ================================================
+    println!("## Pattern Syntax\n");
+
+    println!("|--- Matching Literals");
+    let x = 1;
+    match x {
+        1 => println!("one!"),
+        2 => println!("two!"),
+        3 => println!("three!"),
+        _ => println!("anything!"),
+    }
+    println!();
+
+    // ------------------------------------------------
+    println!("---| Matching Named Variables");
+
+    let x = Some(5);
+    let y = "TEN";
+
+    match x {
+        Some(50) => println!("Got 50"),
+        // This is NOT the same y as above!
+        // This is matching ANY value inside a Some()
+        Some(y) => println!("Matched, y = {y}"),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {y}", x);
+    println!();
+
+    // ------------------------------------------------
+    println!("---| Multiple Patterns");
+    let x = 1;
+
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+    println!();
+
+    // ------------------------------------------------
+    println!("---| Matching Ranges of values with `..=`");
+    let x = 5;
+
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
+    }
+
+    let x = 'c';
+
+    match x {
+        'a'..='z' => println!("lowercase ASCII"),
+        'A'..='Z' => println!("uppercase ASCII"),
+        _ => println!("you got punct"),
+    }
+    println!();
+
+    // ------------------------------------------------
+    println!("---| Destructuring to Break Apart Values");
+
+    println!("   |___| ... Structs");
+    let p = Point { x: 0, y: 7 };
+
+    // One way, with confusing names
+    let Point { x: a, y: b } = p;
+    assert_eq!(0, a);
+    assert_eq!(7, b);
+
+    // More succinct
+    let Point { x, y } = p;
+    assert_eq!(0, x);
+    assert_eq!(7, y);
+
+    match p {
+        Point { x: 0, y: 0 } => println!("At the origin"),
+        Point { x, y: 0 } => println!("On the x axis at {x}"),
+        Point { x: 0, y } => println!("On the y axis at {y}"),
+        Point { x, y } => {
+            println!("On neither axis: ({x}, {y})");
+        }
+    }
+    println!("   |___| ... Enums");
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure");
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {x} and y direction {y}");
+        }
+        Message::Write(text) => {
+            println!("Text message: {text}");
+        }
+        Message::ChangeColor(r, g, b) => {
+            println!("Changing color to {r}, {g}, {b}");
+        }
+        Message::ColorChange(placeholder) => {
+            println!("Meh: {:?}", placeholder);
+        }
+    }
+    println!("   |___| ... Nested Structs and Enums");
+    let msg = Message::ColorChange(Color::Hsv(0, 160, 255));
+
+    match msg {
+        Message::ColorChange(Color::Rgb(r, g, b)) => {
+            println!("Change color to RGB {r}, {g}, {b}");
+        }
+        Message::ColorChange(Color::Hsv(h, s, v)) => {
+            println!("Change color to HSV {h}, {s}, {v}");
+        }
+        _ => (),
+    }
+    println!("   |___| ... Structs and Tuples");
+
+    let ((_feet, _inches), Point { x: _x, y: _y }) = ((3, 10), Point { x: 3, y: -10 });
+    println!();
+
+    // ------------------------------------------------
+    println!("---| Ignoring Values in a Pattern");
 }
